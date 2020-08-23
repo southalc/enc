0615a168e6c98d3044b8cca3d6aa37b7b9f197d7
southalc <southallc@gmail.com>
2020-08-23 16:41:23 -0500

    update to enable empty inventory declarations

--- enc.rb
+++ enc.rb
@@ -5,7 +5,8 @@
 
 require 'yaml'
 
-inventory_file = '/etc/puppetlabs/puppet/inventory.yaml'
+inventory_file = 'inventory.yaml'
+#inventory_file = '/etc/puppetlabs/puppet/inventory.yaml'
 
 class Hash
   def deep_merge(h)
@@ -24,15 +25,16 @@ if inventory.has_key?('nodes')
     node_data = inventory['nodes'][node]
     if node_data.class == String
       if inventory.has_key?(node_data)
+        # node assigned to a valid environment
         output['environment'] = node_data
       else
-        # node assigned to an environment that is not in the inventory
+        # node assigned to an environment not defined in inventory
         output['environment'] = "production"
       end
     elsif node_data.nil?
+      # node present with no data assigned - default to production environment
       output['environment'] = "production"
-    else
-      # node_data is a hash
+    elsif node_data.class == Hash
       if node_data.empty?
         # An empty node hash defaults to the production environment
         output['environment'] = "production"
@@ -44,6 +46,9 @@ if inventory.has_key?('nodes')
         # Add node_data to output
         output = output.merge(node_data)
       end
+    else
+      # node data is invalid - default to production environment
+      output['environment'] = "production"
     end
   else
     # node not present in "nodes" hash - set default environment
@@ -54,9 +59,12 @@ else
   output['environment'] = "production"
 end
 
+
 # Merge environment data with node data
 if inventory.has_key?(output['environment'])
-  output = inventory[output['environment']].deep_merge(output)
+  unless inventory[output['environment']].nil?
+    output = inventory[output['environment']].deep_merge(output)
+  end
 end
 
 puts output.to_yaml
